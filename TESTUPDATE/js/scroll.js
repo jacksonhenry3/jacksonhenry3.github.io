901965db3f8b0cc2f427e2c6c64e5473c09800d8
Jackson Henry <jackson.henry3@gmail.com>
2020-11-17 20:37:18 -0500

    better styles?

--- TESTUPDATE/js/scroll.js
+++ TESTUPDATE/js/scroll.js
@@ -4,7 +4,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+        }, 1000, 'easeInOutExpo');
+        // event.preventDefault();
     });
 });
